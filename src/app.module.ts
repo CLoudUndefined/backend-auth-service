959f66b9d182e34d0e63d49b1ed302016802a9e9
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-21 20:05:47 +0200

    feat(app): register AuthModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ServiceUsersModule } from './service-users/service-users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ServiceUsersModule],
+  imports: [ServiceUsersModule, AuthModule],
 })
 export class AppModule {}
