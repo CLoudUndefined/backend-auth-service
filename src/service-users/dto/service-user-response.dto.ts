5b72c3141b22cecd3b8b66341e31a258562dbc87
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-21 15:30:28 +0200

    feat(service-user): add basic controller endpoints and dtos

---
+++ src/service-users/dto/service-user-response.dto.ts
@@ -0,0 +1,8 @@
+export class ServiceUserResponseDto {
+    id: number;
+    email: string;
+    isGod: boolean;
+    recoveryQuestion: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
--- src/service-users/service-users.controller.ts
+++ src/service-users/service-users.controller.ts
@@ -1,6 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotImplementedException, Get, Put, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { UpdateServiceUserRequestDto } from './dto/update-service-user-request';
+import { ServiceUserResponseDto } from './dto/service-user-response.dto';
+import { DeleteServiceUserResponseDto } from './dto/delete-service-user-response.dto';
 
 @Controller('service-users')
 export class ServiceUsersController {
-    
+    @Get('me')
+    async findMe(): Promise<ServiceUserResponseDto> {
+        throw new NotImplementedException('Logic not implemented yet');
+    }
+
+    @Put('me')
+    async update(@Body() updateDto: UpdateServiceUserRequestDto): Promise<ServiceUserResponseDto> {
+        throw new NotImplementedException('Logic not implemented yet');
+    }
+
+    @Get()
+    async findAll(): Promise<ServiceUserResponseDto[]> {
+        throw new NotImplementedException('Logic not implemented yet');
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: number): Promise<DeleteServiceUserResponseDto> {
+        throw new NotImplementedException('Logic not implemented yet');
+    }
 }
