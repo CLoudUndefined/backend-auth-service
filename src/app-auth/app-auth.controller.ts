f9162850101b5797717f2dc84b709a458045da37
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-23 13:58:41 +0200

    feat(app-auth): add basic controller enpoints and refactor auth dtos to common

--- src/app-auth/app-auth.controller.ts
+++ src/app-auth/app-auth.controller.ts
@@ -1,18 +1,18 @@
 import { Body, Controller, NotImplementedException, Post } from '@nestjs/common';
+import { ChangePasswordDto } from 'src/common/dto/auth/change-password.dto';
+import { LoginResponseDto } from 'src/common/dto/auth/login-response.dto';
+import { LoginDto } from 'src/common/dto/auth/login.dto';
+import { RecoveryAskResponseDto } from 'src/common/dto/auth/recovery-ask-response.dto';
+import { RecoveryAskDto } from 'src/common/dto/auth/recovery-ask.dto';
+import { RecoveryResetDto } from 'src/common/dto/auth/recovery-reset.dto';
+import { RegisterDto } from 'src/common/dto/auth/register.dto';
+import { UpdateRecoveryResponseDto } from 'src/common/dto/auth/update-recovery-response.dto';
+import { UpdateRecoveryDto } from 'src/common/dto/auth/update-recovery.dto';
+import { MessageResponseDto } from 'src/common/dto/message-response.dto';
 import { ServiceUserResponseDto } from 'src/service-users/dto/service-user-response.dto';
-import { RegisterDto } from '../common/dto/auth/register.dto';
-import { LoginDto } from '../common/dto/auth/login.dto';
-import { ChangePasswordDto } from '../common/dto/auth/change-password.dto';
-import { RecoveryAskDto } from '../common/dto/auth/recovery-ask.dto';
-import { RecoveryResetDto } from '../common/dto/auth/recovery-reset.dto';
-import { UpdateRecoveryDto } from '../common/dto/auth/update-recovery.dto';
-import { LoginResponseDto } from '../common/dto/auth/login-response.dto';
-import { RecoveryAskResponseDto } from '../common/dto/auth/recovery-ask-response.dto';
-import { MessageResponseDto } from '../common/dto/message-response.dto';
-import { UpdateRecoveryResponseDto } from '../common/dto/auth/update-recovery-response.dto';
 
-@Controller('auth')
-export class AuthController {
+@Controller('apps/:appId/auth')
+export class AppAuthController {
     @Post('register')
     async register(@Body() registerDto: RegisterDto): Promise<ServiceUserResponseDto> {
         throw new NotImplementedException('Logic not implemented yet');
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ServiceUsersModule } from './service-users/service-users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppsModule } from './apps/apps.module';
+import { AppAuthModule } from './app-auth/app-auth.module';
 
 @Module({
-  imports: [ServiceUsersModule, AuthModule, AppsModule]
+  imports: [ServiceUsersModule, AuthModule, AppsModule, AppAuthModule]
 })
 export class AppModule {}
