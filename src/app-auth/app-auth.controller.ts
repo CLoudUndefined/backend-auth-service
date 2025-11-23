2d0d10be6b2951266b67bb5fb1dd9eac5d6a3936
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-23 15:21:02 +0200

    fix(app-auth) correct response type to AppUserResponseDto

--- src/app-auth/app-auth.controller.ts
+++ src/app-auth/app-auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, NotImplementedException, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { AppUserResponseDto } from 'src/app-users/dto/app-user-response.dto';
 import { ChangePasswordDto } from 'src/common/dto/auth/change-password.dto';
 import { LoginResponseDto } from 'src/common/dto/auth/login-response.dto';
 import { LoginDto } from 'src/common/dto/auth/login.dto';
@@ -9,12 +10,11 @@ import { RegisterDto } from 'src/common/dto/auth/register.dto';
 import { UpdateRecoveryResponseDto } from 'src/common/dto/auth/update-recovery-response.dto';
 import { UpdateRecoveryDto } from 'src/common/dto/auth/update-recovery.dto';
 import { MessageResponseDto } from 'src/common/dto/message-response.dto';
-import { ServiceUserResponseDto } from 'src/service-users/dto/service-user-response.dto';
 
 @Controller('apps/:appId/auth')
 export class AppAuthController {
     @Post('register')
-    async register(@Param('appId', ParseIntPipe) appId: number, @Body() registerDto: RegisterDto): Promise<ServiceUserResponseDto> {
+    async register(@Param('appId', ParseIntPipe) appId: number, @Body() registerDto: RegisterDto): Promise<AppUserResponseDto> {
         throw new NotImplementedException('Logic not implemented yet');
     }
 
