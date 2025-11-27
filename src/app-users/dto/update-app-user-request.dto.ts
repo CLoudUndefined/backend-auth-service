06ccf68bdd13038e78e7125166fa222c28523c48
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-27 10:41:24 +0200

    refactor(app-users): remove roleIds from dto

--- src/app-users/dto/update-app-user-request.dto.ts
+++ src/app-users/dto/update-app-user-request.dto.ts
@@ -1,27 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ArrayNotEmpty, IsArray, IsEmail, IsInt, IsNotEmpty, IsOptional, IsPositive, MaxLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
 
 export class UpdateAppUserRequestDto {
     @ApiProperty({
         example: 'developer@example.com',
         description: 'New email for the user',
-        required: false,
     })
-    @IsOptional()
     @IsNotEmpty()
     @IsEmail()
     @MaxLength(255)
-    email?: string;
-
-    @ApiProperty({
-        example: 2,
-        description: 'ID of the role to assign to this user.',
-        required: false,
-    })
-    @IsOptional()
-    @IsArray()
-    @ArrayNotEmpty()
-    @IsInt({ each: true })
-    @IsPositive({ each: true })
-    roleIds?: number[];
+    email: string;
 }
