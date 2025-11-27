f20bb9cac6defa7e3589a7d8675bc72987a05ab6
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-27 10:46:59 +0200

    feat(app-users): add GetAppUsersQueryDto for filtering users

---
+++ src/app-users/dto/get-app-users-query.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional, IsInt } from "class-validator";
+
+export class GetAppUsersQueryDto {
+    @ApiProperty({
+        example: 1,
+        description: 'Filter by Role ID',
+    })
+    @IsOptional()
+    @IsInt()
+    roleId?: number;
+}
