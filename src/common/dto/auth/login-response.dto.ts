3a699b7ce89fa4ca242d8920d366e491bae38c14
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-27 11:01:30 +0200

    feat(auth): add refreshToken field to LoginResponseDto

--- src/common/dto/auth/login-response.dto.ts
+++ src/common/dto/auth/login-response.dto.ts
@@ -6,4 +6,10 @@ export class LoginResponseDto {
         description: 'JWT Access Token to be used in Authorization header (Bearer)',
     })
     accessToken: string;
+
+    @ApiProperty({
+        example: 'dcc434-82e3-4...',
+        description: 'Refresh Token to obtain a new Access Token when the current one expires',
+    })
+    refreshToken: string;
 }
