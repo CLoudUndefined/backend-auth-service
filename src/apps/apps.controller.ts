307cde39de09474551f815bb91cf0a22f0c80fb9
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-11-27 09:58:15 +0200

    refactor: restructure apps controller for app api

--- src/apps/apps.controller.ts
+++ src/apps/apps.controller.ts
@@ -1,45 +1,20 @@
-import { Body, Controller, Delete, Get, NotImplementedException, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
-import { CreateAppRequestDto } from './dto/create-app-request.dto';
+import { Body, Controller, Get, NotImplementedException, Put } from '@nestjs/common';
 import { AppResponseDto } from './dto/app-response.dto';
 import { UpdateAppRequestDto } from './dto/update-app-request.dto';
-import { MessageResponseDto } from 'src/common/dto/message-response.dto';
-import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateAppResponseDto } from './dto/create-app-response.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Apps (Management)')
 @ApiBearerAuth('JWT-auth-service')
-@Controller('apps')
+@Controller('apps/:appId')
 export class AppsController {
-    @Post()
-    @ApiOperation({
-        summary: 'Create new application',
-        description: 'Creates a new application container with its own secret and user base.',
-    })
-    @ApiResponse({
-        status: 201,
-        description: 'App created successfully',
-        type: AppResponseDto,
-    })
-    @ApiResponse({
-        status: 400,
-        description: 'Validation failed',
-    })
-    @ApiResponse({
-        status: 401,
-        description: 'Unauthorized',
-    })
-    async create(@Body() createAppDto: CreateAppRequestDto): Promise<CreateAppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
-    }
-
     @Get()
-    @ApiOperation({
-        summary: 'Get all apps (God only)',
-        description: 'Returns a list of all applications. Only accessible to users with god-mode privileges',
+    @ApiOperation({ 
+        summary: 'Get current app info',
+        description: 'Returns public details about the application context.',
     })
     @ApiResponse({
         status: 200,
-        description: 'List retrieved',
+        description: 'App details found',
         type: AppResponseDto,
         isArray: true,
     })
@@ -47,47 +22,14 @@ export class AppsController {
         status: 401,
         description: 'Unauthorized',
     })
-    async findAll(): Promise<AppResponseDto[]> {
+    async findOne(): Promise<AppResponseDto[]> {
         throw new NotImplementedException('Logic not implemented yet');
     }
 
-    @Get(':id')
-    @ApiOperation({
-        summary: 'Get app by ID',
-        description: 'Returns details of a specific application.',
-    })
-    @ApiParam({
-        name: 'id',
-        description: 'App ID',
-        example: 1,
-    })
-    @ApiResponse({
-        status: 200,
-        description: 'App details found',
-        type: AppResponseDto,
-    })
-    @ApiResponse({
-        status: 401,
-        description: 'Unauthorized',
-    })
-    @ApiResponse({
-        status: 403,
-        description: 'Forbidden - can only view own apps or requires god-mode',
-    })
-    @ApiResponse({ status: 404, description: 'App not found' })
-    async findOne(@Param('id', ParseIntPipe) id: number): Promise<AppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
-    }
-
-    @Put(':id')
+    @Put()
     @ApiOperation({
         summary: 'Update application',
         description: 'Updates name or description of the application.',
-    })
-    @ApiParam({
-        name: 'id',
-        description: 'App ID',
-        example: 1,
     })
     @ApiResponse({
         status: 200,
@@ -103,48 +45,12 @@ export class AppsController {
         description: 'Unauthorized',
     })
     @ApiResponse({
-        status: 403,
-        description: 'Forbidden - can only view own apps or requires god-mode',
-    })
-    @ApiResponse({
         status: 404,
         description: 'App not found',
     })
     async update(
-        @Param('id', ParseIntPipe) id: number,
         @Body() updateDto: UpdateAppRequestDto,
     ): Promise<AppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
-    }
-
-    @Delete(':id')
-    @ApiOperation({
-        summary: 'Delete application',
-        description: 'Deletes the application and all associated users/roles definitively.',
-    })
-    @ApiParam({
-        name: 'id',
-        description: 'App ID',
-        example: 1,
-    })
-    @ApiResponse({
-        status: 200,
-        description: 'App deleted',
-        type: MessageResponseDto,
-    })
-    @ApiResponse({
-        status: 401,
-        description: 'Unauthorized',
-    })
-    @ApiResponse({
-        status: 403,
-        description: 'Forbidden - can only view own apps or requires god-mode',
-    })
-    @ApiResponse({
-        status: 404,
-        description: 'App not found',
-    })
-    async remove(@Param('id', ParseIntPipe) id: number): Promise<MessageResponseDto> {
         throw new NotImplementedException('Logic not implemented yet');
     }
 }
