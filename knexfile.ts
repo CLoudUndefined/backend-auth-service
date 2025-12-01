37d1ce6a762676bd06a887c3ee4d45b8f1069fae
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-12-01 17:16:04 +0200

    ops: add knexfile and update .env.example with POSTGRES_HOST variable

---
+++ knexfile.ts
@@ -0,0 +1,30 @@
+import { Knex } from 'knex';
+import { knexSnakeCaseMappers } from 'objection';
+
+// TODO: In the future, divide this config into development and production
+const config: Knex.Config = {
+    client: 'postgresql',
+    connection: {
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+    },
+    pool: {
+        min: 1,
+        max: 5,
+    },
+    migrations: {
+        directory: './src/database/migrations',
+        extension: 'ts',
+        tableName: 'knex_migrations',
+    },
+    seeds: {
+        directory: './src/database/seeds',
+        extension: 'ts',
+    },
+    ...knexSnakeCaseMappers(),
+};
+
+export default config;
