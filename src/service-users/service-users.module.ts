75fa33ecf0941f8e61f38192d78680e8d30d1a64
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-12-19 20:19:01 +0200

    feat(service-user): add ServiceUsersService with basic operations

--- src/service-users/service-users.module.ts
+++ src/service-users/service-users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ServiceUsersController } from './service-users.controller';
+import { ServiceUsersService } from './service-users.service';
 
 @Module({
     controllers: [ServiceUsersController],
+    providers: [ServiceUsersService],
 })
 export class ServiceUsersModule {}
