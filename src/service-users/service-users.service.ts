ff8e1f8be55a429b732a7a9731553e478292e4fe
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-12-19 20:19:01 +0200

    feat(service-user): add ServiceUsersService with basic operations

---
+++ src/service-users/service-users.service.ts
@@ -0,0 +1,29 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { ServiceUsersRepository } from 'src/database/repositories/service-users.repository';
+import * as bcrypt from 'bcrypt';
+import type { ServiceUserModel } from 'src/database/models/service-user.model';
+
+@Injectable()
+export class ServiceUsersService {
+    constructor(private readonly serviceUsersRepository: ServiceUsersRepository) {}
+
+    async create(email: string, plainPassword: string): Promise<ServiceUserModel> {
+        const existingUser = await this.serviceUsersRepository.findByEmail(email);
+
+        if (existingUser) {
+            throw new ConflictException('User with this email already exists');
+        }
+
+        const passwordHash = await bcrypt.hash(plainPassword, 10);
+
+        return await this.serviceUsersRepository.create(email, passwordHash, true);
+    }
+
+    async findByEmail(email: string): Promise<ServiceUserModel | undefined> {
+        return await this.serviceUsersRepository.findByEmail(email);
+    }
+
+    async findById(id: number): Promise<ServiceUserModel | undefined> {
+        return await this.serviceUsersRepository.findById(id);
+    }
+}
