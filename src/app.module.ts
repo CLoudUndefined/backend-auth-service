abc495fa13f240eab6fda4f810eced0cc4ac6760
Тимофей Рудаков <timofejrud.11@gmail.com>
2025-12-30 12:54:45 +0200

    chore: add env validation with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,20 @@ import { AppRolesModule } from './app-roles/app-roles.module';
 import { AppPermissionsModule } from './app-permissions/app-permissions.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
             envFilePath: '.env',
+            validationSchema: Joi.object({
+                POSTGRES_PORT: Joi.number().port().default(5432),
+                POSTGRES_HOST: Joi.string().default('localhost'),
+                POSTGRES_USER: Joi.string().required(),
+                POSTGRES_PASSWORD: Joi.string().required(),
+                POSTGRES_DB: Joi.string().default('backend_auth_db'),
+            }),
         }),
         ServiceUsersModule,
         AuthModule,
