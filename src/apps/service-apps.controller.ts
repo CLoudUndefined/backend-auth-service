e3f5425cf303eef7bcf20256002f333302d4cfc6
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-07 13:16:28 +0200

    feat(apps): implement service-apps controller endpoints

--- src/apps/service-apps.controller.ts
+++ src/apps/service-apps.controller.ts
@@ -1,16 +1,24 @@
-import { Body, Controller, Delete, Get, NotImplementedException, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
 import { CreateAppRequestDto } from './dto/create-app-request.dto';
 import { AppResponseDto } from './dto/app-response.dto';
 import { UpdateAppRequestDto } from './dto/update-app-request.dto';
 import { MessageResponseDto } from 'src/common/api/dto/message-response.dto';
 import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateAppResponseDto } from './dto/create-app-response.dto';
+import { JwtServiceAuthGuard } from 'src/auth/guards/jwt-service-auth.guard';
+import { AppsService } from './apps.service';
+import { ServiceUser } from 'src/common/decorators/service-user.decorator';
+import { ServiceUserModel } from 'src/database/models/service-user.model';
+import { IsGodGuard } from 'src/auth/guards/is-god.guard';
 
 @ApiTags('Service (Apps Management)')
 @ApiBearerAuth('JWT-auth-service')
 @Controller('service/apps')
 export class ServiceAppsController {
+    constructor(private readonly appsService: AppsService) {}
+
     @Post()
+    @UseGuards(JwtServiceAuthGuard)
     @ApiOperation({
         summary: 'Create new application',
         description: 'Creates a new application container with its own secret and user base.',
@@ -28,11 +36,15 @@ export class ServiceAppsController {
         status: 401,
         description: 'Unauthorized',
     })
-    async create(@Body() createAppDto: CreateAppRequestDto): Promise<CreateAppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
+    async createApp(
+        @ServiceUser() user: ServiceUserModel,
+        @Body() createAppDto: CreateAppRequestDto,
+    ): Promise<CreateAppResponseDto> {
+        return this.appsService.create(user.id, createAppDto);
     }
 
     @Get()
+    @UseGuards(JwtServiceAuthGuard, IsGodGuard)
     @ApiOperation({
         summary: 'Get all apps (God only)',
         description: 'Returns a list of all applications. Only accessible to users with god-mode privileges',
@@ -47,11 +59,12 @@ export class ServiceAppsController {
         status: 401,
         description: 'Unauthorized',
     })
-    async findAll(): Promise<AppResponseDto[]> {
-        throw new NotImplementedException('Logic not implemented yet');
+    async findAllApps(): Promise<AppResponseDto[]> {
+        return this.appsService.findAllApps();
     }
 
     @Get(':id')
+    @UseGuards(JwtServiceAuthGuard)
     @ApiOperation({
         summary: 'Get app by ID',
         description: 'Returns details of a specific application.',
@@ -74,11 +87,15 @@ export class ServiceAppsController {
         description: 'Forbidden - can only view own apps or requires god-mode',
     })
     @ApiResponse({ status: 404, description: 'App not found' })
-    async findOne(@Param('id', ParseIntPipe) id: number): Promise<AppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
+    async findAppById(
+        @ServiceUser() user: ServiceUserModel,
+        @Param('id', ParseIntPipe) id: number,
+    ): Promise<AppResponseDto> {
+        return this.appsService.findAppById(user.id, user.isGod, id);
     }
 
     @Put(':id')
+    @UseGuards(JwtServiceAuthGuard)
     @ApiOperation({
         summary: 'Update application',
         description: 'Updates name or description of the application.',
@@ -108,14 +125,16 @@ export class ServiceAppsController {
         status: 404,
         description: 'App not found',
     })
-    async update(
+    async updateApp(
+        @ServiceUser() user: ServiceUserModel,
         @Param('id', ParseIntPipe) id: number,
-        @Body() updateDto: UpdateAppRequestDto,
+        @Body() updateAppDto: UpdateAppRequestDto,
     ): Promise<AppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
+        return this.appsService.update(user.id, user.isGod, id, updateAppDto);
     }
 
     @Delete(':id')
+    @UseGuards(JwtServiceAuthGuard)
     @ApiOperation({
         summary: 'Delete application',
         description: 'Deletes the application and all associated users/roles definitively.',
@@ -141,8 +160,12 @@ export class ServiceAppsController {
         status: 404,
         description: 'App not found',
     })
-    async remove(@Param('id', ParseIntPipe) id: number): Promise<MessageResponseDto> {
-        console.log('application was deleted :>> ', { appId: id });
-        throw new NotImplementedException('Logic not implemented yet');
+    async deleteApp(
+        @ServiceUser() user: ServiceUserModel,
+        @Param('id', ParseIntPipe) id: number,
+    ): Promise<MessageResponseDto> {
+        await this.appsService.delete(user.id, user.isGod, id);
+
+        return { message: 'Application deleted successfully' };
     }
 }
