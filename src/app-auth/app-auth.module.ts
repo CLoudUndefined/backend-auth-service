377ba88e4baa71b10d599f4a527d00afd59e8ea3
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-14 11:04:32 +0200

    chore(app-auth): configure passport and jwt configuration

--- src/app-auth/app-auth.module.ts
+++ src/app-auth/app-auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppAuthController } from './app-auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StringValue } from 'ms';
 
 @Module({
+    imports: [
+        PassportModule.register({ defaultStrategy: 'jwt-app' }),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                signOptions: {
+                    expiresIn: configService.getOrThrow<StringValue>('JWT_ACCESS_TOKEN_EXPIRES_IN'),
+                },
+            }),
+        }),
+    ],
     controllers: [AppAuthController],
 })
 export class AppAuthModule {}
