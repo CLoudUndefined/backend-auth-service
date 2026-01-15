c326f55aeaa762311c281c74201fc9bb830ce5f7
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-15 17:23:53 +0200

    feat(app-auth): implement user registration with recovery questions

--- src/app-auth/service/app-auth.service.ts
+++ src/app-auth/service/app-auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { AppUsersRepository } from 'src/database/repositories/app-users.repository';
+import * as bcrypt from 'bcrypt';
+import { ApplicationUserModel } from 'src/database/models/application-user.model';
 
 @Injectable()
-export class AppAuthService {}
+export class AppAuthService {
+    constructor(private readonly appUsersRepository: AppUsersRepository) {}
+
+    async register(
+        appId: number,
+        email: string,
+        plainPassword: string,
+        recoveryQuestion?: string,
+        recoveryAnswer?: string,
+    ): Promise<ApplicationUserModel> {
+        const existingUser = await this.appUsersRepository.exists(appId, email);
+
+        if (existingUser) {
+            throw new ConflictException('User with this email alreadt exists');
+        }
+
+        const passwordHash = await bcrypt.hash(plainPassword, 10);
+
+        const user = await this.appUsersRepository.create(appId, email, passwordHash);
+
+        if (recoveryQuestion && recoveryAnswer) {
+            const answerHash = await bcrypt.hash(recoveryAnswer, 10);
+            await this.appUsersRepository.createRecovery(user.id, recoveryQuestion, answerHash);
+        }
+
+        return user;
+    }
+}
