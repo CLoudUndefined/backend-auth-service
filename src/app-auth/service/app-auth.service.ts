1bb3c21a5cebc2fa4db0e4affa51c7d9d590377b
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-16 15:10:27 +0200

    feat(app-auth): implement full auth flow for app-users

--- src/app-auth/service/app-auth.service.ts
+++ src/app-auth/service/app-auth.service.ts
@@ -1,4 +1,5 @@
 import {
+    BadRequestException,
     ConflictException,
     ForbiddenException,
     Injectable,
@@ -15,6 +16,7 @@ import { AppsRepository } from 'src/database/repositories/apps.repository';
 import { EncryptionService } from 'src/encryption/encryption.service';
 import { ConfigService } from '@nestjs/config';
 import ms, { StringValue } from 'ms';
+import { RecoveryQuestionListDto } from 'src/common/service/dto/auth/recovery-question-list.dto';
 
 @Injectable()
 export class AppAuthService {
@@ -51,7 +53,7 @@ export class AppAuthService {
         }
 
         if (existingUser) {
-            throw new ConflictException('User with this email alreadt exists');
+            throw new ConflictException('User with this email already exists');
         }
 
         const passwordHash = await bcrypt.hash(plainPassword, 10);
@@ -101,5 +103,237 @@ export class AppAuthService {
         );
 
         return { accessToken, refreshToken };
+    }
+
+    async changePassword(appId: number, userId: number, oldPassword: string, newPassword: string): Promise<void> {
+        const [app, user] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByIdInApp(appId, userId),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        if (oldPassword === newPassword) {
+            throw new BadRequestException('New password must be different');
+        }
+
+        await this.verifyCredentialOrThrow(oldPassword, user.passwordHash);
+
+        const newPasswordHash = await bcrypt.hash(newPassword, 10);
+
+        await this.appUsersRepository.update(userId, { passwordHash: newPasswordHash });
+
+        await this.appUsersRepository.deleteAllUserRefreshTokens(userId);
+    }
+
+    async refreshToken(appId: number, refreshToken: string): Promise<AuthTokensDto> {
+        const tokenHash = crypto.createHash('sha256').update(refreshToken).digest('hex');
+
+        const [app, storedToken] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findRefreshTokenByHash(tokenHash),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!storedToken) {
+            throw new NotFoundException('Invalid refresh token');
+        }
+
+        if (new Date() > storedToken.expiresAt) {
+            await this.appUsersRepository.deleteRefreshToken(storedToken.id);
+            throw new UnauthorizedException('Refresh token expired');
+        }
+
+        const user = await this.appUsersRepository.findByIdInApp(appId, storedToken.userId);
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        if (user.isBanned) {
+            throw new ForbiddenException('User is banned');
+        }
+
+        const accessToken = this.jwtService.sign(
+            { appId, sub: user.id, email: user.email },
+            { secret: this.encryptionService.decrypt(app.encryptedSecret) },
+        );
+
+        const newRefreshToken = crypto.randomBytes(64).toString('hex');
+        const refreshTokenHash = crypto.createHash('sha256').update(newRefreshToken).digest('hex');
+
+        await this.appUsersRepository.createRefreshToken(
+            user.id,
+            refreshTokenHash,
+            new Date(Date.now() + ms(this.configService.getOrThrow<StringValue>('JWT_REFRESH_TOKEN_EXPIRES_IN', '7d'))),
+        );
+
+        return { accessToken, refreshToken };
+    }
+
+    async addRecovery(appId: number, userId: number, recoveryQuestion: string, recoveryAnswer: string): Promise<void> {
+        const [app, user] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByIdInApp(appId, userId),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user) {
+            throw new NotFoundException('User not found in this application');
+        }
+
+        const answerHash = await bcrypt.hash(recoveryAnswer, 10);
+        await this.appUsersRepository.createRecovery(userId, recoveryQuestion, answerHash);
+    }
+
+    async listRecovery(appId: number, userId: number): Promise<RecoveryQuestionListDto> {
+        const [app, user] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByIdInApp(appId, userId),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user) {
+            throw new NotFoundException('User not found in this application');
+        }
+        const recoveries = await this.appUsersRepository.findRecoveriesByUserId(userId);
+
+        return {
+            questions: recoveries.map((recovery) => {
+                return { id: recovery.id, question: recovery.question };
+            }),
+        };
+    }
+
+    async askRecoveryQuestions(appId: number, email: string): Promise<RecoveryQuestionListDto> {
+        const [app, user] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByEmailInApp(appId, email),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        const recoveries = await this.appUsersRepository.findRecoveriesByUserId(user.id);
+
+        return {
+            questions: recoveries.map((recovery) => {
+                return { id: recovery.id, question: recovery.question };
+            }),
+        };
+    }
+
+    async resetPasswordByRecovery(
+        appId: number,
+        recoveryId: number,
+        email: string,
+        recoveryAnswer: string,
+        newPassword: string,
+    ): Promise<void> {
+        const [app, user, recovery] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByEmailInApp(appId, email),
+            this.appUsersRepository.findRecoveryById(recoveryId),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user || !recovery) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        if (recovery.userId !== user.id) {
+            throw new ForbiddenException('This recovery question does not belong to this user');
+        }
+
+        await this.verifyCredentialOrThrow(recoveryAnswer, recovery.answerHash);
+
+        const newPasswordHash = await bcrypt.hash(newPassword, 10);
+
+        await this.appUsersRepository.update(user.id, { passwordHash: newPasswordHash });
+
+        await this.appUsersRepository.deleteAllUserRefreshTokens(user.id);
+    }
+
+    async updateRecovery(
+        appId: number,
+        userId: number,
+        recoveryId: number,
+        currentPassword: string,
+        newRecoveryQuestion: string,
+        newRecoveryAnswer: string,
+    ): Promise<void> {
+        const [app, user, recovery] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByIdInApp(appId, userId),
+            this.appUsersRepository.findRecoveryById(recoveryId),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user || !recovery) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        if (recovery.userId !== user.id) {
+            throw new ForbiddenException('This recovery question does not belong to this user');
+        }
+
+        await this.verifyCredentialOrThrow(currentPassword, user.passwordHash);
+
+        const newAnswerHash = await bcrypt.hash(newRecoveryAnswer, 10);
+
+        await this.appUsersRepository.updateRecovery(recoveryId, {
+            question: newRecoveryQuestion,
+            answerHash: newAnswerHash,
+        });
+    }
+
+    async removeRecovery(appId: number, userId: number, recoveryId: number, currentPassword: string): Promise<void> {
+        const [app, user, recovery] = await Promise.all([
+            this.appsRepository.findById(appId),
+            this.appUsersRepository.findByIdInApp(appId, userId),
+            this.appUsersRepository.findRecoveryById(recoveryId),
+        ]);
+
+        if (!app) {
+            throw new NotFoundException('Application not found');
+        }
+
+        if (!user || !recovery) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        if (recovery.userId !== user.id) {
+            throw new ForbiddenException('This recovery question does not belong to this user');
+        }
+
+        await this.verifyCredentialOrThrow(currentPassword, user.passwordHash);
+
+        await this.appUsersRepository.deleteRecovery(recoveryId);
     }
 }
