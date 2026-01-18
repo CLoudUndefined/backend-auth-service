6114634d953f025bb7cd758f6ee07d1dff8a322d
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-18 16:11:09 +0200

    feat(app-roles): implement service for app-roles management

--- src/app-roles/app-roles.module.ts
+++ src/app-roles/app-roles.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppRolesController } from './api/app-roles.controller';
 import { ServiceAppRolesController } from './api/service-app-roles.controller';
+import { AppRolesService } from './service/app-roles.service';
 
 @Module({
     controllers: [AppRolesController, ServiceAppRolesController],
+    providers: [AppRolesService],
 })
 export class AppRolesModule {}
