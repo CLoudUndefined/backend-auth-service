693d1ca257dfb742f7ea51e9106a62baaf405556
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-19 06:46:35 +0200

    feat(apps): implement for app-user apps controller

--- src/apps/api/apps.controller.ts
+++ src/apps/api/apps.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Get, NotImplementedException, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Put, UseGuards } from '@nestjs/common';
 import { AppResponseDto } from './dto/app-response.dto';
 import { UpdateAppRequestDto } from './dto/update-app-request.dto';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AppsService } from '../service/apps.service';
+import { JwtAppAuthGuard } from 'src/app-auth/guards/jwt-app-auth.guard';
+import { AppUser } from 'src/common/decorators/app-user.decorator';
+import { type AuthenticatedAppUser } from 'src/app-auth/interfaces/authenticated-app-user.interface';
+import { AppPermissionGuard } from 'src/app-auth/guards/app-permissions.guard';
+import { Permissions } from 'src/app-auth/decorators/permissions.reflector';
 
 @ApiTags('Apps (Management)')
 @ApiBearerAuth('JWT-auth-app')
 @Controller('apps/:appId')
 export class AppsController {
+    constructor(private readonly appsService: AppsService) {}
+
     @Get()
+    @UseGuards(JwtAppAuthGuard, AppPermissionGuard)
+    @Permissions('app.manage')
     @ApiOperation({
         summary: 'Get current app info',
         description: 'Returns public details about the application context.',
@@ -25,11 +35,17 @@ export class AppsController {
         status: 404,
         description: 'App not found',
     })
-    async findOne(): Promise<AppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
+    async getApp(
+        @AppUser() user: AuthenticatedAppUser,
+        @Param('appId', ParseIntPipe) appId: number,
+    ): Promise<AppResponseDto> {
+        const app = await this.appsService.findAppByIdByAppUser(appId, user.id);
+        return new AppResponseDto(app);
     }
 
     @Put()
+    @UseGuards(JwtAppAuthGuard, AppPermissionGuard)
+    @Permissions('app.manage')
     @ApiOperation({
         summary: 'Update application',
         description: 'Updates name or description of the application.',
@@ -51,7 +67,12 @@ export class AppsController {
         status: 404,
         description: 'App not found',
     })
-    async update(@Body() updateDto: UpdateAppRequestDto): Promise<AppResponseDto> {
-        throw new NotImplementedException('Logic not implemented yet');
+    async updateApp(
+        @AppUser() user: AuthenticatedAppUser,
+        @Param('appId', ParseIntPipe) appId: number,
+        @Body() updateAppDto: UpdateAppRequestDto,
+    ): Promise<AppResponseDto> {
+        const app = await this.appsService.updateByAppUser(appId, user.id, updateAppDto.name, updateAppDto.description);
+        return new AppResponseDto(app);
     }
 }
