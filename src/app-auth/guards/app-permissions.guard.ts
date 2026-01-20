a6fcda71761691da3d379216e1a5812dbcf402f4
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-20 15:29:23 +0200

    refactor(app-auth): remove roles from jwt and move DB role check from jwt to permission guard

--- src/app-auth/guards/app-permissions.guard.ts
+++ src/app-auth/guards/app-permissions.guard.ts
@@ -1,12 +1,16 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthenticatedAppUser } from '../interfaces/authenticated-app-user.interface';
+import { AppUsersRepository } from 'src/database/repositories/app-users.repository';
 
 @Injectable()
 export class AppPermissionGuard implements CanActivate {
-    constructor(private readonly reflector: Reflector) {}
+    constructor(
+        private readonly reflector: Reflector,
+        private readonly appUsersRepository: AppUsersRepository,
+    ) {}
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         const requiredPermissions = this.reflector.get<string[]>('permissions', context.getHandler());
 
         if (!requiredPermissions || requiredPermissions.length === 0) {
@@ -17,11 +21,17 @@ export class AppPermissionGuard implements CanActivate {
         const user = request.user as AuthenticatedAppUser;
 
         if (!user) {
-            throw new UnauthorizedException('User not found');
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const appUser = await this.appUsersRepository.findByIdInAppWithRolesAndPermissions(user.appId, user.id);
+
+        if (!appUser) {
+            throw new UnauthorizedException('Invalid credentials');
         }
 
         const appUserPermissions =
-            user.roles?.flatMap((role) => role.permissions?.map((permission) => permission.name) || []) || [];
+            appUser.roles?.flatMap((role) => role.permissions?.map((permission) => permission.name) || []) || [];
         const hasPermission = requiredPermissions.every((permission) => appUserPermissions.includes(permission));
 
         if (!hasPermission) {
--- src/app-auth/interfaces/authenticated-app-user.interface.ts
+++ src/app-auth/interfaces/authenticated-app-user.interface.ts
@@ -1,7 +1,4 @@
-import { ApplicationRoleWithPermissionsModel } from 'src/types/application-role.types';
-
 export interface AuthenticatedAppUser {
     appId: number;
     id: number;
-    roles: ApplicationRoleWithPermissionsModel[];
 }
