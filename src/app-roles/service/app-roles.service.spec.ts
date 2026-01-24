7340c241233bf7bad359f280accabf8f6419e695
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-24 14:53:54 +0200

    test(app-roles): add AppRolesService.updateRole unit tests

--- src/app-roles/service/app-roles.service.spec.ts
+++ src/app-roles/service/app-roles.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppRolesService } from './app-roles.service';
 import { AppRolesRepository } from 'src/database/repositories/app-roles.repository';
 import { AppPermissionsRepository } from 'src/database/repositories/app-permissions.repository';
-import { ConflictException, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, NotFoundException } from '@nestjs/common';
 
 describe('AppRolesService', () => {
     let service: AppRolesService;
@@ -185,6 +185,187 @@ describe('AppRolesService', () => {
             const result = await service.getRole(appId, roleId);
 
             expect(mockAppRolesRepository.findByIdWithPermissionsInApp).toHaveBeenCalledWith(appId, roleId);
+
+            expect(result).toEqual(role);
+        });
+    });
+
+    describe('updateRole', () => {
+        const appId = 1;
+        const roleId = 2;
+        const existingPermissionIds = [1, 2, 3];
+        const role = { id: roleId, name: 'mock-role-name', description: 'mock-role-description', permissions: [] };
+        const roleWithPermissions = {
+            id: roleId,
+            name: 'mock-role-name',
+            description: 'mock-role-description',
+            permissions: [{ id: 1 }, { id: 2 }, { id: 3 }],
+        };
+        const updatedRoleName = { id: roleId, name: 'mock-new-role-name', description: 'mock-role-description' };
+        const updatedRoleDescription = { id: roleId, name: 'mock-role-name', description: 'mock-new-role-description' };
+        const updatedRolePermissions = {
+            id: roleId,
+            name: 'mock-role-name',
+            description: 'mock-role-description',
+            permissions: [{ id: 1 }, { id: 2 }, { id: 3 }],
+        };
+        const updatedRoleAll = {
+            id: roleId,
+            name: 'mock-new-role-name',
+            description: 'mock-new-role-description',
+            permissions: [{ id: 1 }, { id: 2 }, { id: 3 }],
+        };
+
+        it('should throw NotFoundException if role not found', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(null);
+
+            await expect(service.updateRole(appId, roleId, 'mock-new-role-name')).rejects.toThrow(NotFoundException);
+
+            expect(mockAppRolesRepository.findByIdInApp).toHaveBeenCalledWith(appId, roleId);
+        });
+
+        it('should throw BadRequestException if no fields provided', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+
+            await expect(service.updateRole(appId, roleId)).rejects.toThrow(BadRequestException);
+
+            expect(mockAppRolesRepository.findByIdInApp).toHaveBeenCalledWith(appId, roleId);
+        });
+
+        it('should throw ConflictException if new name already exists', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppRolesRepository.existsByNameInApp.mockResolvedValue(true);
+
+            await expect(service.updateRole(appId, roleId, 'mock-new-role-name')).rejects.toThrow(ConflictException);
+
+            expect(mockAppRolesRepository.existsByNameInApp).toHaveBeenCalledWith(appId, 'mock-new-role-name');
+        });
+
+        it('should not check name conflict if name unchanged', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppRolesRepository.update.mockResolvedValue(updatedRoleDescription);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(updatedRoleDescription);
+
+            await service.updateRole(appId, roleId, 'mock-role-name', 'mock-new-role-description');
+
+            expect(mockAppRolesRepository.existsByNameInApp).not.toHaveBeenCalled();
+
+            expect(mockAppRolesRepository.update).toHaveBeenCalledWith(roleId, {
+                name: 'mock-role-name',
+                description: 'mock-new-role-description',
+            });
+        });
+
+        it('should throw NotFoundException if some permissions do not exist', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppPermissionsRepository.findExistingIds.mockResolvedValue(existingPermissionIds);
+
+            await expect(service.updateRole(appId, roleId, undefined, undefined, [1, 2, 3, 4])).rejects.toThrow(
+                NotFoundException,
+            );
+
+            expect(mockAppRolesRepository.findByIdInApp).toHaveBeenCalledWith(appId, roleId);
+            expect(mockAppPermissionsRepository.findExistingIds).toHaveBeenCalledWith([1, 2, 3, 4]);
+        });
+
+        it('should throw NotFoundException if updated role not found', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppRolesRepository.existsByNameInApp.mockResolvedValue(false);
+            mockAppRolesRepository.update.mockResolvedValue(updatedRoleName);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(undefined);
+
+            await expect(service.updateRole(appId, roleId, 'mock-new-role-name')).rejects.toThrow(NotFoundException);
+
+            expect(mockAppRolesRepository.update).toHaveBeenCalledWith(roleId, {
+                name: 'mock-new-role-name',
+                description: undefined,
+            });
+            expect(mockAppRolesRepository.findByIdWithPermissionsInApp).toHaveBeenCalledWith(appId, roleId);
+        });
+
+        it('should successfully update only name', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppRolesRepository.existsByNameInApp.mockResolvedValue(false);
+            mockAppRolesRepository.update.mockResolvedValue(updatedRoleName);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(updatedRoleName);
+
+            const result = await service.updateRole(appId, roleId, 'mock-new-role-name');
+
+            expect(mockAppRolesRepository.update).toHaveBeenCalledWith(roleId, {
+                name: 'mock-new-role-name',
+                description: undefined,
+            });
+            expect(mockAppRolesRepository.setPermissions).not.toHaveBeenCalled();
+
+            expect(result).toEqual(updatedRoleName);
+        });
+        it('should successfully update only description', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppRolesRepository.existsByNameInApp.mockResolvedValue(false);
+            mockAppRolesRepository.update.mockResolvedValue(updatedRoleDescription);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(updatedRoleDescription);
+
+            const result = await service.updateRole(appId, roleId, undefined, 'mock-new-role-description');
+
+            expect(mockAppRolesRepository.update).toHaveBeenCalledWith(roleId, {
+                name: undefined,
+                description: 'mock-new-role-description',
+            });
+            expect(mockAppRolesRepository.setPermissions).not.toHaveBeenCalled();
+
+            expect(result).toEqual(updatedRoleDescription);
+        });
+
+        it('should successfully update only permissions', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppPermissionsRepository.findExistingIds.mockResolvedValue(existingPermissionIds);
+            mockAppRolesRepository.setPermissions.mockResolvedValue(undefined);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(updatedRolePermissions);
+
+            const result = await service.updateRole(appId, roleId, undefined, undefined, [1, 2, 3]);
+
+            expect(mockAppRolesRepository.update).not.toHaveBeenCalled();
+            expect(mockAppPermissionsRepository.findExistingIds).toHaveBeenCalledWith([1, 2, 3]);
+            expect(mockAppRolesRepository.setPermissions).toHaveBeenCalledWith(roleId, [1, 2, 3]);
+
+            expect(result).toEqual(updatedRolePermissions);
+        });
+
+        it('should successfully update name, description and permissions', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(role);
+            mockAppRolesRepository.existsByNameInApp.mockResolvedValue(false);
+            mockAppPermissionsRepository.findExistingIds.mockResolvedValue([1, 2]);
+            mockAppRolesRepository.update.mockResolvedValue(undefined);
+            mockAppRolesRepository.setPermissions.mockResolvedValue(undefined);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(updatedRoleAll);
+
+            const result = await service.updateRole(
+                appId,
+                roleId,
+                'mock-new-role-name',
+                'mock-new-role-description',
+                [1, 2],
+            );
+
+            expect(mockAppRolesRepository.update).toHaveBeenCalledWith(roleId, {
+                name: 'mock-new-role-name',
+                description: 'mock-new-role-description',
+            });
+            expect(mockAppRolesRepository.setPermissions).toHaveBeenCalledWith(roleId, [1, 2]);
+
+            expect(result).toEqual(updatedRoleAll);
+        });
+
+        it('should successfully clear all permissions if an empty array is provided', async () => {
+            mockAppRolesRepository.findByIdInApp.mockResolvedValue(roleWithPermissions);
+            mockAppRolesRepository.setPermissions.mockResolvedValue(undefined);
+            mockAppRolesRepository.findByIdWithPermissionsInApp.mockResolvedValue(role);
+
+            const result = await service.updateRole(appId, roleId, undefined, undefined, []);
+
+            expect(mockAppRolesRepository.update).not.toHaveBeenCalled();
+            expect(mockAppPermissionsRepository.findExistingIds).not.toHaveBeenCalled();
+            expect(mockAppRolesRepository.setPermissions).toHaveBeenCalledWith(roleId, []);
 
             expect(result).toEqual(role);
         });
