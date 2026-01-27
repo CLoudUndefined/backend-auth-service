15bb7827132348ce1e55b2b86ffbff76679ba52c
Тимофей Рудаков <timofejrud.11@gmail.com>
2026-01-27 17:58:56 +0200

    feat(app-roles): add graphql schema for app-roles and generate types

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -7,6 +6,18 @@
 
 /* tslint:disable */
 /* eslint-disable */
+
+export class CreateAppRoleInput {
+    name: string;
+    description?: Nullable<string>;
+    permissionIds?: Nullable<number[]>;
+}
+
+export class UpdateAppRoleInput {
+    name?: Nullable<string>;
+    description?: Nullable<string>;
+    permissionIds?: Nullable<number[]>;
+}
 
 export class AppPermission {
     id: number;
@@ -19,6 +30,27 @@ export abstract class IQuery {
     abstract permissions(): AppPermission[] | Promise<AppPermission[]>;
 
     abstract permission(id: string): Nullable<AppPermission> | Promise<Nullable<AppPermission>>;
+
+    abstract appRoles(): AppRole[] | Promise<AppRole[]>;
+
+    abstract appRole(id: number): AppRole | Promise<AppRole>;
+}
+
+export class AppRole {
+    id: number;
+    name: string;
+    description?: Nullable<string>;
+    permissions: AppPermission[];
+    createdAt: string;
+    updatedAt: string;
+}
+
+export abstract class IMutation {
+    abstract createAppRole(input: CreateAppRoleInput): AppRole | Promise<AppRole>;
+
+    abstract updateAppRole(id: number, input: UpdateAppRoleInput): AppRole | Promise<AppRole>;
+
+    abstract deleteAppRole(id: number): boolean | Promise<boolean>;
 }
 
 type Nullable<T> = T | null;
